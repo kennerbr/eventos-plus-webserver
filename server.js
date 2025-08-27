301a929643b73e080463dc7c60a5f890259d229c
kennerBR <lucasdalhavalhecorrea@gmail.com>
2025-08-27 15:08:47 -0300

    Ajuste nas rotas e no servidor.

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const path = require("node:path")
 const router = require("./src/routes/index.js")
+const dotenv = require("dotenv")
+dotenv.config()
+
+const PORT = process.env.PORT || 3333
 
 const app = express()
 app.use(express.urlencoded({extended: true}))
@@ -10,6 +14,6 @@ app.use(express.static(path.join(__dirname, "./src/public")))
 
 app.use("/", router)
 
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta /");
+app.listen(PORT, () => {
+    console.log("Servidor rodando na porta "+PORT);
 })
